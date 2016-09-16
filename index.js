7d33420ab12f2648b444d54401527ec642d9d5ab
Debarun Pal <debarunpal@live.com>
2016-09-16 10:09:19 +0530

    changed generic message structure

--- index.js
+++ index.js
@@ -185,51 +185,49 @@ function callSendAPI(messageData) {
   });
 }
 
-function sendGenericMessage(senderID) {
-    let messageData = {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "generic",
-                "elements": [{
-                    "title": "Place an order",
-                    "subtitle": "Order your favorite clothing today",
-                    "image_url": "https://cdn2.iconfinder.com/data/icons/windows-8-metro-style/512/shoping_cart.png",
-                    "buttons": [{
-                        "type": "web_url",
-                        "url": "http://www.qroods.com/",
-                        "title": "View Website"
-                    }, {
-                        "type": "postback",
-                        "title": "Order",
-                        "payload": "Your order has been successfully placed. Congrats!!!",
-                    }],
-                }, {
-                    "title": "Track your order",
-                    "subtitle": "Track your latest order",
-                    "image_url": "https://cdn4.iconfinder.com/data/icons/maps-and-navigation-solid-icons-vol-3/72/111-512.png",
-                    "buttons": [{
-                        "type": "postback",
-                        "title": "Track",
-                        "payload": "Your shipment is on the way...",
-                    }],
-                }]
-            }
+function sendGenericMessage(recipientId) {
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      attachment: {
+        type: "template",
+        payload: {
+          template_type: "generic",
+          elements: [{
+            title: "rift",
+            subtitle: "Next-generation virtual reality",
+            item_url: "https://www.oculus.com/en-us/rift/",
+            image_url: "http://messengerdemo.parseapp.com/img/rift.png",
+            buttons: [{
+              type: "web_url",
+              url: "https://www.oculus.com/en-us/rift/",
+              title: "Open Web URL"
+            }, {
+              type: "postback",
+              title: "Call Postback",
+              payload: "Payload for first bubble",
+            }],
+          }, {
+            title: "touch",
+            subtitle: "Your Hands, Now in VR",
+            item_url: "https://www.oculus.com/en-us/touch/",
+            image_url: "http://messengerdemo.parseapp.com/img/touch.png",
+            buttons: [{
+              type: "web_url",
+              url: "https://www.oculus.com/en-us/touch/",
+              title: "Open Web URL"
+            }, {
+              type: "postback",
+              title: "Call Postback",
+              payload: "Payload for second bubble",
+            }]
+          }]
         }
+      }
     }
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token:token},
-        method: 'POST',
-        json: {
-            recipient: {id:sender},
-            message: messageData,
-        }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', error)
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
-        }
-    })
+  };
+
+  callSendAPI(messageData);
 }
