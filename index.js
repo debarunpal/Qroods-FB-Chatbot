3f533dc7097610b6db374afbe4ac5bb113acc867
Debarun Pal <debarunpal@live.com>
2016-09-16 13:16:15 +0530

    Reverted changes

--- index.js
+++ index.js
@@ -152,30 +152,12 @@ function receivedMessage(event) {
 */
 function sendTextMessage(recipientId, messageText) {
     var messageData = {
-      "recipient":{
-      "id":"USER_ID"
-    },
-    "message":{
-      "attachment":{
-        "type":"template",
-        "payload":{
-          "template_type":"button",
-          "text":"What do you want to do next?",
-          "buttons":[
-            {
-              "type":"web_url",
-              "url":"https://petersapparel.parseapp.com",
-              "title":"Show Website"
-            },
-            {
-              "type":"postback",
-              "title":"Start Chatting",
-              "payload":"USER_DEFINED_PAYLOAD"
-            }
-          ]
-        }
+        recipient: {
+        id: recipientId
+      },
+      message: {
+        text: messageText
       }
-    }
     };
 
     callSendAPI(messageData);
