e259908d5665cda271763abb98340f2271161d71
Debarun Pal <debarunpal@live.com>
2016-09-16 14:10:20 +0530

    Introduced button welcome

--- index.js
+++ index.js
@@ -156,9 +156,27 @@ function sendTextMessage(recipientId, messageText) {
         id: recipientId
       },
       message: {
-        text: messageText
+          attachment:{
+          type:"template",
+          payload:{
+            template_type:"button",
+            text:"What do you want to do next?",
+            buttons:[
+              {
+                type:"web_url",
+                url:"https://petersapparel.parseapp.com",
+                title:"Show Website"
+              },
+              {
+                type:"postback",
+                title:"Start Chatting",
+                payload:"USER_DEFINED_PAYLOAD"
+              }
+            ]
+          }
+        }
       }
-    };
+  };
 
     callSendAPI(messageData);
 }
